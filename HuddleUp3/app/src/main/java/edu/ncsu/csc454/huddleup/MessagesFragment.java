e9acb64618997590bd2b5a513b7a9f2fcde08f72
nikki603 <nicole@Nicoles-MacBook-Air.local>
2015-04-19 15:10:31 -0400

    added floating circle button to Messages view

--- HuddleUp3/app/src/main/java/edu/ncsu/csc454/huddleup/MessagesFragment.java
+++ HuddleUp3/app/src/main/java/edu/ncsu/csc454/huddleup/MessagesFragment.java
@@ -1,6 +1,5 @@
 package edu.ncsu.csc454.huddleup;
 
-//import info.androidhive.tabsswipe.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,10 +11,23 @@ public class MessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        //set view template
         View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
 
+        //add action to circle "plus" button
+        View addButton = rootView.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewMessage();
+            }
+        });
+
         return rootView;
+    }
+
+    public void addNewMessage() {
+
     }
 
 }
